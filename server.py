0765ec1960387ec9bdbd8f0e2e06472c7701741f
MarcoXZh <zxu3@ualberta.ca>
2014-02-22 23:06:23 -0700

    First step

--- server.py
+++ server.py
@@ -22,79 +22,101 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 import json
 app = Flask(__name__)
 app.debug = True
 
 # An example world
 # {
-#    'a':{'x':1, 'y':2},
-#    'b':{'x':2, 'y':3}
+#   'a':{'x':1, 'y':2},
+#   'b':{'x':2, 'y':3}
 # }
 
 class World:
-    def __init__(self):
-        self.clear()
-        
-    def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
-        entry[key] = value
-        self.space[entity] = entry
+  def __init__(self):
+    self.clear()
+  # def __init__(self)
 
-    def set(self, entity, data):
-        self.space[entity] = data
+  def update(self, entity, key, value):
+    entry = self.space.get(entity, dict())
+    entry[key] = value
+    self.space[entity] = entry
+  # def update(self, entity, key, value)
 
-    def clear(self):
-        self.space = dict()
+  def set(self, entity, data):
+    self.space[entity] = data
+  # def set(self, entity, data)
 
-    def get(self, entity):
-        return self.space.get(entity,dict())
-    
-    def world(self):
-        return self.space
+  def clear(self):
+    self.space = dict()
+  # def clear(self)
+
+  def get(self, entity):
+    return self.space.get(entity, dict())
+  # def get(self, entity)
+
+  def world(self):
+    return self.space
+  # def world(self)
+
+  def __str__(self):
+    msg = 'World: {\n'
+    for key in self.space:
+      msg += '  dict["%"] = "%"\n' % (key, self.space[key])
+    msg += '}'
+    return msg
+  # def __unicode__(self)
+# class World
 
 # you can test your webservice from the commandline
 # curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
 
-myWorld = World()          
+myWorld = World()
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
 def flask_post_json():
-    '''Ah the joys of frameworks! They do so much work for you
-       that they get in the way of sane operation!'''
-    if (request.json != None):
-        return request.json
-    elif (request.data != None and request.data != ''):
-        return json.loads(request.data)
-    else:
-        return json.loads(request.form.keys()[0])
+  '''Ah the joys of frameworks! They do so much work for you
+     that they get in the way of sane operation!'''
+  if (request.json != None):
+    return request.json
+  elif (request.data != None and request.data != ''):
+    return json.loads(request.data)
+  else:
+    return json.loads(request.form.keys()[0])
+  # else - if (request.json != None)
+# def flask_post_json()
 
 @app.route("/")
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+  return redirect(url_for('static', filename='index.html'))
+# def hello()
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+  '''update the entities via this interface'''
+  return None
+# def update(entity)
 
-@app.route("/world", methods=['POST','GET'])    
+@app.route("/world", methods=['POST','GET'])
 def world():
-    '''you should probably return the world here'''
-    return None
+  return str(myWorld).replace('\n', '<br />')
+# def world()
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+  '''This is the GET version of the entity interface, return a representation of the entity'''
+  return None
+# def get_entity(entity)
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
-    '''Clear the world out!'''
-    return None
+  '''Clear the world out!'''
+  return None
+# def clear()
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
+# if __name__ == "__main__"
+
